95af4c2913df5a20c26f856b19e216bfd5d3ac35
Peter Zignego <pzignego@gmail.com>
2023-01-26 16:31:10 -0500

    ubuntu, centos, amazon

--- src/swift/install.sh
+++ src/swift/install.sh
@@ -76,108 +76,190 @@ find_version_from_git_tags() {
     echo "${variable_name}=${!variable_name}"
 }
 
-apt_get_update()
-{
+apt_get_update() {
     if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
         apt-get update -y
     fi
 }
+
+yum_update() {
+    if [ "$(find /var/lib/yum/history/$(date -I) | wc -l)" = "0" ]; then
+        echo "Running yum update..."
+        yum update -y
+    fi
+}
+
 # Checks if packages are installed and installs them if not
-check_packages() {
+check_packages_apt() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         apt_get_update
         apt-get -y install --no-install-recommends "$@"
     fi
 }
 
-export DEBIAN_FRONTEND=noninteractive
+# Checks if packages are installed and installs them if not
+check_packages_yum() {
+    if ! rpm -q "$@" > /dev/null 2>&1; then
+        yum_update
+        yum -y install "$@"
+    fi
+}
 
-# shared packages
-check_packages \
-    binutils \
-    curl \
-    git \
-    libc6-dev \
-    libncurses6 \
-    libedit2 \
-    libsqlite3-0 \
-    pkg-config \
-    tzdata \
-    zlib1g-dev \
-    gnupg2 \
-    ca-certificates
-
-# version specific packages
-# Get the version of Debian
-# Ubuntu version  Debian version
-# 22.10 kinetic	  bookworm/ sid - 12
-# 22.04 jammy     bookworm/sid
-# 21.10 impish    bullseye/sid - 11
-# 21.04 hirsute   bullseye/sid
-# 20.10 groovy    bullseye/sid
-# 20.04 focal     bullseye/sid
-# 19.10 eoan      buster / sid - 10
-# 19.04 disco     buster / sid
-# 18.10 cosmic    buster / sid
-# 18.04 bionic    buster / sid
-DEBIAN=$(cat /etc/debian_version)
-PLATFORM=""
-if [[ "${DEBIAN:0:2}" == "12" ]]; then
-    PLATFORM="ubuntu22.04"
-    check_packages \
-        libcurl4-openssl-dev \
-        libgcc-9-dev \
-        libpython3.8 \
-        libsqlite3-0 \
-        libstdc++-9-dev \
-        libxml2-dev \
-        libz3-dev \
-        unzip
-elif [[ "${DEBIAN:0:2}" == "11" ]]; then
-    PLATFORM="ubuntu20.04"
-    check_packages \
+# Ubuntu 18.04, 20.04, 22.04
+install_debian_packages() {
+    local version=$1
+    # Ensure apt is in non-interactive to avoid prompts
+    export DEBIAN_FRONTEND=noninteractive
+    # Shared packages
+    check_packages_apt \
+        binutils \
+        curl \
+        git \
         libc6-dev \
-        libcurl4 \
-        libgcc-9-dev \
-        libpython2.7 \
-        libstdc++-9-dev \
-        libxml2 \
-        libz3-dev \
-        uuid-dev
-elif [[ "${DEBIAN:0:2}" == "10" ]]; then
-    PLATFORM="ubuntu18.04"
-    check_packages \
-        libcurl4 \
-        libgcc-5-dev \
-        libpython2.7 \
+        libedit2 \
         libsqlite3-0 \
-        libstdc++-5-dev \
-        libxml2
+        pkg-config \
+        tzdata \
+        zlib1g-dev \
+        gnupg2 \
+        ca-certificates
+
+    if [ "${version}" == "22.04" ]; then
+        check_packages_apt \
+            libcurl4-openssl-dev \
+            libgcc-9-dev \
+            libpython3.8 \
+            libsqlite3-0 \
+            libstdc++-9-dev \
+            libxml2-dev \
+            libz3-dev \
+            unzip
+    elif [ "${version}" == "20.04" ]; then
+        check_packages_apt \
+            libc6-dev \
+            libcurl4 \
+            libgcc-9-dev \
+            libpython2.7 \
+            libstdc++-9-dev \
+            libxml2 \
+            libz3-dev \
+            uuid-dev
+    elif [ "${version}" == "18.04" ]; then
+        check_packages_apt \
+            libcurl4 \
+            libgcc-5-dev \
+            libpython2.7 \
+            libsqlite3-0 \
+            libstdc++-5-dev \
+            libxml2
+    else
+        echo "Unsupported OS: ${PRETTY_NAME}"
+        exit 1
+    fi
+
+    # Clean up
+    rm -rf /var/lib/apt/lists/*
+}
+
+# Amazon Linux 2
+install_amazon_packages() {
+    local version=$1
+
+    if [ "${version}" == "2" ]; then
+        check_packages_yum \
+            binutils \
+            gcc \
+            git \
+            glibc-static \
+            gzip \
+            libbsd \
+            libcurl \
+            libedit \
+            libicu \
+            libsqlite \
+            libstdc++-static \
+            libuuid \
+            libxml2 \
+            tar \
+            tzdata \
+            gnupg2 \
+            ca-certificates
+    else
+        echo "Unsupported OS: ${PRETTY_NAME}"
+        exit 1
+    fi
+}
+
+# CentOS 7
+install_centos_packages() {
+    local version=$1
+
+    if [ "${version}" == "7" ]; then
+        check_packages_yum \
+            binutils \
+            gcc \
+            git \
+            glibc-static \
+            libbsd-devel \
+            libedit \
+            libedit-devel \
+            libicu-devel \
+            libstdc++-static \
+            pkg-config \
+            python2 \
+            sqlite
+
+        # __block conflicts with clang's __block qualifier
+        sed -i -e 's/\*__block/\*__libc_block/g' /usr/include/unistd.h
+    else
+        echo "Unsupported OS: ${PRETTY_NAME}"
+        exit 1
+    fi
+}
+
+# Bring in ID, VERSION_ID 
+. /etc/os-release
+
+if [[ "${ID}" == "ubuntu" ]]; then
+    install_debian_packages "${VERSION_ID}"
+elif [[ "${ID}" == "centos" ]]; then
+    install_centos_packages "${VERSION_ID}"
+elif [[ "${ID}" == "amzn" ]]; then
+    install_amazon_packages "${VERSION_ID}"
 else
-    echo "Unsupported debian version."
+    echo "Unsupported OS: ${PRETTY_NAME}"
     exit 1
 fi
 
-# Verify arch/version compatibility
+# We use these to construct the download link
 architecture="$(uname -m)"
+platform="${ID}${VERSION_ID}"
+# Special case for Amazon Linux 2...
+if [[ "${ID}" == "amzn" ]]; then
+    platform="amazonlinux2"
+fi
+
+# Verify architecture compatibility
 if [[ "${architecture}" == "aarch64" ]]; then
-    if [[ "${DEBIAN:0:2}" == "10" ]]; then
-        echo "Use Debian 11 or later for aarch64."
+    if [ "${platform}" == "ubuntu18.04" ] || [ "${platform}" == "centos7" ]; then
+        echo "aarch64 is not supported on ${PRETTY_NAME}."
         exit 1
     fi
-    PLATFORM="${PLATFORM}-${architecture}"
-elif [[ "${architecture}" == "x86_64" ]]; then
+    platform="${platform}-${architecture}"
+    echo "Supported architecture ${architecture}."
+elif  [[ "${architecture}" == "x86_64" ]]; then
     echo "Supported architecture ${architecture}."
 else
     echo "Unsupported architecture "${architecture}"."
     exit 1
 fi
-platform_stripped="$(echo "${PLATFORM}" | tr -d '.')"
+
+platform_stripped="$(echo "${platform}" | tr -d '.')"
 
 find_version_from_git_tags SWIFT_VERSION "https://github.com/apple/swift" "refs/tags/swift-"
 
-download_link="https://download.swift.org/swift-${SWIFT_VERSION}-release/$platform_stripped/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-${PLATFORM}.tar.gz"
+download_link="https://download.swift.org/swift-${SWIFT_VERSION}-release/$platform_stripped/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-${platform}.tar.gz"
 
 # Install Swift
 if [[ "${SWIFT_VERSION}" != "none" ]] && [[ "$(swift --version)" != *"${SWIFT_VERSION}"* ]]; then
@@ -188,25 +270,22 @@ if [[ "${SWIFT_VERSION}" != "none" ]] && [[ "$(swift --version)" != *"${SWIFT_VE
     exit_code=$?
     set -e
     if [ "$exit_code" != "0" ]; then
-        echo "(!) Download failed."
+        echo "Download failed."
         exit 1
     fi
     # verify gpg signature
     curl -fsSL "${SWIFT_GPG_KEY_URI}" | gpg --import -
     gpg --keyserver hkp://keyserver.ubuntu.com --refresh-keys Swift
-    gpg --verify /tmp/swift.tar.gz.sig || (echo "Failed to verify the GPG signature of swift-${SWIFT_VERSION}-RELEASE-${PLATFORM}.tar.gz"; exit 1)
+    gpg --verify /tmp/swift.tar.gz.sig || (echo "Failed to verify the GPG signature of swift-${SWIFT_VERSION}-RELEASE-${platform}.tar.gz"; exit 1)
     # unpack
     tar xzf /tmp/swift.tar.gz -C /usr/local/bin
     # clean up
     rm -rf /tmp/swift.tar.gz
     rm -rf /tmp/swift.tar.gz.sig
-    export PATH=/usr/local/bin/swift-${SWIFT_VERSION}-RELEASE-${PLATFORM}/usr/bin:"${PATH}"
+    export PATH=/usr/local/bin/swift-${SWIFT_VERSION}-RELEASE-${platform}/usr/bin:"${PATH}"
     echo "$(swift --version)"
 else
     echo "Swift is already installed with version ${SWIFT_VERSION}. Skipping."
 fi
-
-# Clean up
-rm -rf /var/lib/apt/lists/*
 
 echo "Done!"
